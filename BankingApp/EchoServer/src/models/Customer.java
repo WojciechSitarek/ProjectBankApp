7a74bea03c3f37105cd3fa2921a80180b13dfa7f
lol19992002 <matmels@wp.pl>
2024-03-05 14:29:01 +0100

    Update Customer.java
    yyy chwilowo tyle zrobiłem

--- BankingApp/EchoServer/src/models/Customer.java
+++ BankingApp/EchoServer/src/models/Customer.java
@@ -12,9 +12,60 @@ public class Customer {
 
     // toString()
 
+
+    private String name;
+    private String lastname;
+    private String login;
+    private String password;
+    private String address;
     private int customerId;
+    private int phoneNumber;
+
+
+
+
+//    public Customer(String login, String password){
+//        this.login = login;
+//        this.password = password;
+//    }
+//    public Customer(String name, String lastname, int phoneNumber, String address){
+//
+//    }
+
+
+
+    
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getAddress() {
+        return address;
+    }
 
     public int getCustomerId() {
         return customerId;
     }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+
+
 }
+
