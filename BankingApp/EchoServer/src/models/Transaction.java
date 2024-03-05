6b5414a7896c25a54b956899d283bac28dde8faa
lol19992002 <matmels@wp.pl>
2024-03-05 14:56:03 +0100

    Update Transaction.java

--- BankingApp/EchoServer/src/models/Transaction.java
+++ BankingApp/EchoServer/src/models/Transaction.java
@@ -10,4 +10,65 @@ public class Transaction {
 
     //toString - transactionInfo.
 
+
+    private double transactionAmount;
+    private int customerAccountNumber;
+    private int destinationAccountNumber;
+    private int transactionId;
+
+
+    public double getTransactionAmount() {
+        return transactionAmount;
+    }
+
+    public void setTransactionAmount(double transactionAmount) {
+        this.transactionAmount = transactionAmount;
+    }
+
+
+
+    public int getCustomerAccountNumber() {
+        return customerAccountNumber;
+    }
+
+    public void setCustomerAccountNumber(int customerAccountNumber) {
+        this.customerAccountNumber = customerAccountNumber;
+    }
+
+
+
+    public int getDestinationAccountNumber() {
+        return destinationAccountNumber;
+    }
+
+    public void setDestinationAccountNumber(int destinationAccountNumber) {
+        this.destinationAccountNumber = destinationAccountNumber;
+    }
+
+
+
+    public int getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(int transactionId) {
+        this.transactionId = transactionId;
+    }
+
+
+
+    public Transaction(int customerAccountNumber, int destinationAccountNumber){
+        this.customerAccountNumber = customerAccountNumber;
+        this.destinationAccountNumber = destinationAccountNumber;
+        this.transactionAmount = 0;
+    }
+
+
+
+    public void getTransactionInfo() {
+        String transInfo = String.format("Customer [Account number: %s, Amount: %s, Destination Account number: %s, TransactionId: %s]",
+                this.customerAccountNumber, this.transactionAmount,this.destinationAccountNumber, this.transactionId);
+        System.out.println(transInfo);
+    }
+
 }
